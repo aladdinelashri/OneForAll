db4dc7a82aec0cf77e7fd9abcd4bef7c273c80bd
aladdinelashri <13434593+aladdinelashri@users.noreply.github.com>
2022-08-12 05:04:09 +0200

    main page layout

---
+++ resources/views/adminpage2.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+            <!-- Page Heading -->
+
+            <!-- Page Content -->
+            <main>
+                <div class="flex flex-col w-56 bg-white h-full overflow-hidden">
+                    <div class="flex items-center justify-center h-16 shadow-md">
+                      <h1 class="text-3xl uppercase text-indigo-400">Logo</h1>
+                    </div>
+                    <ul class="flex flex-col py-4">
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-home"></i></span>
+                          <span class="text-sm font-medium">Dashboard</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-music"></i></span>
+                          <span class="text-sm font-medium">Music</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-drink"></i></span>
+                          <span class="text-sm font-medium">Drink</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-shopping-bag"></i></span>
+                          <span class="text-sm font-medium">Shopping</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-chat"></i></span>
+                          <span class="text-sm font-medium">Chat</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-user"></i></span>
+                          <span class="text-sm font-medium">Profile</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-bell"></i></span>
+                          <span class="text-sm font-medium">Notifications</span>
+                          <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-log-out"></i></span>
+                          <span class="text-sm font-medium">Logout</span>
+                        </a>
+                      </li>
+                    </ul>
+                  
+
+                  </div>
+
+                
+            </main>
+        </div>
+   
+    </body>
+    <div class="contaier">
+    <h1 class="text-3xl uppercase text-indigo-400">Logo</h1>
+    <div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/adminpage1', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('adminpage1');
+});
+
+Route::get('/adminpage2', function () {
+    return view('adminpage2');
+});
+
+
+Route::get('/adminpage3', function () {
+    return view('adminpage3');
+});
+
+Route::get('/adminpage4', function () {
+    return view('adminpage4');
+});
+
+Route::get('/adminpage5', function () {
+    return view('adminpage5');
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::group(['middleware' =>'auth'], function () {
+       Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
+        Route::resource('pages', \App\Http\Controllers\Admin\PageController::class)
+            ->only(['edit', 'update']);
+        
+        Route::get('users', [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
+
+       
+    });
+});
+
+
+
 
 require __DIR__.'/auth.php';
